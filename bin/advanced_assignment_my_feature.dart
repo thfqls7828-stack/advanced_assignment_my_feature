3082e78ade2ca8153be663bfa1b27947570745fe
glory <thfqls7828@gmail.com>
2025-12-10 12:05:01 +0900

    init : 나만의 기능 추가

---
+++ bin/advanced_assignment_my_feature.dart
@@ -0,0 +1,7 @@
+import 'package:advanced_assignment_my_feature/load_user_data.dart';
+
+void main(List<String> arguments) {
+  const filePath = "lib/models/student.txt";
+
+  loadUserData(filePath);
+}
